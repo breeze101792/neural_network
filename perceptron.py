c6acd1725c5504c598df96586651c815f9e50c8e
Shaowu <breeze101792@gmail.com>
2015-10-31 21:05:45 +0800

    add cell_sigmoid class

--- perceptron.py
+++ perceptron.py
@@ -2,35 +2,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import math
-'''
-def extended(ax, x, y, **args):
+import random
+from data_proc import data_proc as dp
 
-    xlim = ax.get_xlim()
-    ylim = ax.get_ylim()
-
-    x_ext = np.linspace(xlim[0], xlim[1], 100)
-    p = np.polyfit(x, y , deg=1)
-    y_ext = np.poly1d(p)(x_ext)
-    ax.plot(x_ext, y_ext, **args)
-    ax.set_xlim(xlim)
-    ax.set_ylim(ylim)
-    return ax
-'''
 class cell:
-    def __init__(self, num_in = 2, learning_rate = 0.4):
-        self.num_in = num_in
+    def __init__(self, dimensions = 2, learning_rate = 0.4):
+        self.dimensions = dimensions
         self.learning_rate = learning_rate
         self.xs = 0
         self.y = 0
         self.weights = [-1]
         self.in_cells = []
         self.out_cells = []
-        for i in range(0, self.num_in):
+        self.weight_init()
+    def weight_init(self):
+        for i in range(0, self.dimensions):
             self.weights.append(0 if i % 2 == 0 else 1)
-        #print("hello")
     def summation(self):
         x = 0
-        for i in range(0, self.num_in + 1):
+        for i in range(0, self.dimensions + 1):
             x += self.weights[i] * self.xs[i]
         #print("sum")
         #print(self.weights,", ", self.xs,", ", x)
@@ -64,6 +54,17 @@ class cell:
     def getweights(self):
         return self.weights
 
+class cell_sigmoid(cell):
+    def __init__(self, dimensions = 2, learning_rate = 0.5):
+        super().__init__(dimensions = dimensions, learning_rate = learning_rate)
+    def weight_init(self):
+        for i in range(0, self.dimensions):
+            self.weights.append(random.random())
+    def activation(self):
+        stimulates = self.summation()
+        #print("stimulation = ", stimulates)
+        self.y = (1 - math.e ** (-2 * self.v_para *  stimulates))/(1 + math.e ** (-2 * self.v_para * stimulates))
+        return self.y
 
 class perceptron:
     def __init__(self, dimension = 2, learning_rate = 0.4, training_times = 10, judge = 1, best_w = False):
@@ -114,6 +115,7 @@ class perceptron:
         total = 0.0
         points = testing_set[0]
         ys = testing_set[1]
+        # print("test", points, ys)
         for point, y in zip(points, ys):
             #print(input)
             total += 1.0
@@ -125,134 +127,26 @@ class perceptron:
 
     def test(self, point):
         return self.cells.stimulation(point[0:-1])
-'''
-class perceptron_single:
-    def __init__(self, data, class_num, learning_rate = 0.4, training_times = 10, best_w = False):
-        self.class_num = class_num
-        self.data = data
-        self.perceptrons = []
-        self.weights = []
-        self.learning_rate = learning_rate
-        self.training_times = training_times
-        self.best_w = best_w
-    def training(self):
-        for i in range(self.class_num):
-            self.perceptrons.append(perceptron(self.data, dimension = 2, learning_rate = self.learning_rate, training_times = self.training_times, judge = i, best_w = self.best_w))
-            self.perceptrons[i].training()
-            self.weights.append(self.perceptrons[i].get_weights())
-    def get_weights(self):
-        return self.weights
-    def get_itimes(self):
-        x = 0
-        for idx, i in enumerate(self.perceptrons):
-            if x < i.get_itimes():
-                 x = idx
-        return self.perceptrons[idx].get_itimes()
-    def get_best_result(self):
-        x = 0
-        for idx,i in enumerate(self.perceptrons):
-            if x < i.best_d[1]:
-                 x = idx
-        return self.perceptrons[idx].best_d
-    def get_err_rate(self):
-        count = 0
-        number = 0
-        for point in self.data:
-            ans = True
-            print("new point", point)
-            for idx, perc in enumerate(self.perceptrons):
-                x = perc.test(point)
-                print(ans, ", ", x, ", ", idx)
-                if ans == True and x == 1 and idx != point[-1]:
-                    ans = False
-                elif ans == True and x == -1 and idx == point[-1]:
-                    ans == False
-            number += 1
-            if ans == True:
-                count += 1
-        return 1 - count / number
 
-class cell_sigmoid(cell):
-    def __init__(self, num_in = 2, learning_rate = 0.4, v_para = 1):
-        super().__init__(num_in = 2, learning_rate = 0.4, v_para = 1)
-        self.v_para = v_para
-    def activation(self):
-        stimulates = self.summation()
-        #print("stimulation = ", stimulates)
-        self.y = (1 - math.e ** (-2 * self.v_para *  stimulates))/(1 + math.e ** (-2 * self.v_para * stimulates))
-        return self.y
 
-class perceptron_m:
-    def __init__(self, data, num_classes = 1, dimension = 2, learning_rate = 0.4, training_times = 10):
-        self.cells = []
-        self.num_classes = num_classes
-        self.learning_rate = learning_rate
-        self.training_times = training_times
-        self.data = data
-        self.dimension = dimension
-        self.err_rate = 0
-        for _ in range(0, num_classes):
-            self.cells.append(cell_sigmoid(dimension, learning_rate))
 
-    def training(self):
-        for _ in range(0, self.training_times):
-            for test in self.data:
-                for idx, each_cell in enumerate(self.cells, start=0):
-
-                    print(idx, ":test point", test)
-                    x = each_cell.stimulation(test[0:-1])
-                    print("feedback-> ",x, test[-1])
-                    if idx == test[-1] and x > 0:
-                        each_cell.feedback()
-                    elif idx != test[-1] and x <= 0:
-                        each_cell.feedback()
-                    else:
-                        #print(test, ", ",each_cell.y)
-                        print("Success sort")
-    def get_weights(self):
-        pass
-        return self.cells.getweights()
-    def get_err_rate(self):
-        pass
-        count = 0.0
-        total = 0.0
-
-        for test in self.data:
-            total += 1
-            prob_c = -1
-            prob_max = -1
-            for idx, each_cell in enumerate(self.cells, start=0):
-                x = each_cell.stimulation(test[0:-1])
-                if idx == 0 or prob_max < x:
-                    prob_c = idx
-                    prob_max = x
-            if test[-1] == prob_c:
-                count += 1
-        self.err_rate = count / total
-        return self.err_rate
-'''
-
-def get_data(fn):
-    data = []
-    with open(fn) as f:
-        for line in f:
-            data.append([float(num) for num in line.split()])
-    return data
 
 def main():
     print("Percetpron Test!!!")
-    #data = get_data("./dataset/2Hcircle1.txt")
+    data = dp("./dataset/2Hcircle1.txt")
     #data = get_data("./dataset/2CloseS3.txt")
-    data = get_data("./dataset/C3D.TXT")
+    # data = dp("./dataset/C3D.TXT")
     #data = [[0,0,1],[0,1,1],[1,0,-1],[1,1,1]]
-    '''
-    ne = perceptron(data,3, 0.2, 100)
-    ne.training()
-    print("Err Rate: ", ne.get_err_rate() * 100)
-    '''
-    mnn = perceptron_m(data, 4, 3, training_times = 1)
-    mnn.training()
-    print("Err Rate: ", mnn.get_err_rate() * 100)
+
+    test = cell_sigmoid()
+    # data.open_file()
+    # data.get_data()
+    # print(data.get_data())
+    #
+    #
+    # ne = perceptron(dimension = 2, learning_rate = 0.4, training_times = 30, judge = 1, best_w = False)
+    # ne.training(tmp)
+    # print("Err Rate: ", ne.get_err_rate(tmp) * 100)
 
 
 if __name__ == '__main__':
