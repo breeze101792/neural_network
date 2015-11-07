07a7a180e511845af8b700fe30bd73e818d4eb0d
Shaowu <breeze101792@gmail.com>
2015-11-07 23:41:22 +0800

    add draw_2d_point method

--- ploting.py
+++ ploting.py
@@ -1,11 +1,54 @@
 #!/usr/bin/env python3
 from data_proc import data_proc as dp
+from gi.repository.GdkPixbuf import Pixbuf
+import matplotlib.pyplot as plt
+import numpy as np
 #draw 2d: points, lines, axises
 class paper:
-    def __init__(self, type = 2):
+    def __init__(self, pix_buf = None):
         self.color = ["r","b","g","c","m","y","k","w"]
         self.data = None
-        self.type = type
+        self.fig = plt.figure()
+        self.pix_buf = pix_buf
+    def drawing(self):
+        fig = plt.figure(figsize=(100,100),dpi=75)
+        ax1 = fig.add_axes([0.1, 0.1, 0.4, 0.7])
+        ax2 = fig.add_axes([0.55, 0.1, 0.4, 0.7])
+        x = np.arange(0.0, 2.0, 0.02)
+        y1 = np.sin(2*np.pi*x)
+        y2 = np.exp(-x)
+        l1, l2 = ax1.plot(x, y1, 'rs-', x, y2, 'go')
+
+        y3 = np.sin(4*np.pi*x)
+        y4 = np.exp(-2*x)
+        l3, l4 = ax2.plot(x, y3, 'yd-', x, y4, 'k^')
+
+        fig.legend((l1, l2), ('Line 1', 'Line 2'), 'upper left')
+        # fig.legend((l3, l4), ('Line 3', 'Line 4'), 'upper right')
+        # plt.show()
+        print(fig.get_figure())
+        self.pix_buf = fig.get_figure()
+    # point = [[x1, y1], [x2, y2], ......., [class]]
+    def draw_2d_point(self, dataset):
+        data_point = dataset[0]
+        data_y = dataset[1]
+        tmp_point = []
+        class_list = []
+        for point, y in zip(data_point, data_y):
+            if y not in class_list:
+                class_list.append(y)
+                tmp_point.append([[point[0]],[point[1]]])
+            else:
+                tmp_point[class_list.index(y)][0].append(point[0])
+                tmp_point[class_list.index(y)][1].append(point[1])
+        print(tmp_point)
+
+        for idx in range(len(class_list)):
+            plt.plot(tmp_point[idx][0], tmp_point[idx][1], self.color[idx] + "o")
+        plt.draw()
+        plt.show()
+
+
     def __draw_2d(self):
         plt.ion()
         points = self.training_set[0]
@@ -77,6 +120,12 @@ class paper:
 
 def main():
     print("drawing function")
+    data = dp('/home/shaowu/code/neural_network/dataset/2Ccircle1.txt')
+    data.open_file()
+    buf = None
+    p = paper(buf)
+    print(data.get_data())
+    p.draw_2d_point(data.get_data())
 
 if __name__ == '__main__':
     main()
