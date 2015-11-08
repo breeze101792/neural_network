9609cd5d6d21aaa9b8a3157572c6bc04e685e97d
Shaowu <breeze101792@gmail.com>
2015-11-08 22:14:46 +0800

    add ploting test driver

--- ploting.py
+++ ploting.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
+from gi.repository import Gtk
+from matplotlib.backends.backend_gtk3cairo import FigureCanvasGTK3Cairo as FigureCanvas
 from data_proc import data_proc as dp
 from gi.repository.GdkPixbuf import Pixbuf
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.figure import Figure
 #draw 2d: points, lines, axises
 class paper:
     def __init__(self, pix_buf = None):
         self.color = ["r","b","g","c","m","y","k","w"]
         self.data = None
-        self.fig = plt.figure()
+        # self.fig = plt.figure()
         self.pix_buf = pix_buf
+
+        self.fig = Figure(figsize=(10,10), dpi=100)
+        self.ax = self.fig.add_subplot(111)
+        self.canvas = FigureCanvas(self.fig)
+
     def drawing(self):
         fig = plt.figure(figsize=(100,100),dpi=75)
         ax1 = fig.add_axes([0.1, 0.1, 0.4, 0.7])
@@ -23,13 +31,24 @@ class paper:
         y4 = np.exp(-2*x)
         l3, l4 = ax2.plot(x, y3, 'yd-', x, y4, 'k^')
 
-        fig.legend((l1, l2), ('Line 1', 'Line 2'), 'upper left')
+        # fig.legend((l1), ("test"), 'upper right')
         # fig.legend((l3, l4), ('Line 3', 'Line 4'), 'upper right')
-        # plt.show()
+        plt.show()
         print(fig.get_figure())
-        self.pix_buf = fig.get_figure()
-    # point = [[x1, y1], [x2, y2], ......., [class]]
+    def resetpaper(self):
+        self.ax.cla()
+        # self.ax.set_xlim(0,10)
+        # self.ax.set_ylim(0,10)
+        self.ax.grid(True)
+
+    def draw(self):
+        pass
+        # self.fig.canvas.draw()
+        # plt.draw()
+        # plt.show()
+
     def draw_2d_point(self, dataset):
+        self.resetpaper()
         data_point = dataset[0]
         data_y = dataset[1]
         tmp_point = []
@@ -41,80 +60,26 @@ class paper:
             else:
                 tmp_point[class_list.index(y)][0].append(point[0])
                 tmp_point[class_list.index(y)][1].append(point[1])
-        print(tmp_point)
 
         for idx in range(len(class_list)):
-            plt.plot(tmp_point[idx][0], tmp_point[idx][1], self.color[idx] + "o")
-        plt.draw()
-        plt.show()
+            self.ax.plot(tmp_point[idx][0], tmp_point[idx][1], self.color[idx] + "o")
+        self.fig.canvas.draw()
 
-
-    def __draw_2d(self):
-        plt.ion()
-        points = self.training_set[0]
-        ys = self.training_set[1]
-        for point, y in zip(points, ys):
-            if y == 0:
-                plt.plot(point[0], point[1] ,color[0] + "o")
-            elif y == 1:
-                plt.plot(point[0], point[1] ,color[1] + "o")
-            elif y == 2:
-                plt.plot(point[0], point[1] ,color[2] + "o")
-            elif point[-1] == 3:
-                plt.plot(point[0], point[1] ,color[3] + "o")
-            elif point[-1] == 4:
-                plt.plot(point[0], point[1] ,color[4] + "o")
-            elif point[-1] == 5:
-                plt.plot(point[0], point[1] ,color[5] + "o")
-            elif point[-1] == 6:
-                plt.plot(point[0], point[1] ,color[6] + "o")
-
-        #plt.plot([2,0,-2,0], [0,2,0,-2] ,"wo")
-        print(self.weights)
-        if self.weights == 1:
-            x = np.linspace(plt.xlim()[0], plt.xlim()[1], 2)
-            y = np.linspace(plt.ylim()[0], plt.ylim()[1], 2)
-            if self.weights[1] == 0:
-                y = [self.weights[0]/self.weights[2],self.weights[0]/self.weights[2]]
-            elif self.weights[2] == 0:
-                x = [self.weights[0]/self.weights[1],self.weights[0]/self.weights[1]]
-            else:
-                #x = [0,self.weights[0]/self.weights[1]]
-                print("draw")
-                if -self.weights[1]/self.weights[2] > 1:
-                    print(" > ")
-                    x = (self.weights[0] - self.weights[2] * x) / self.weights[1]
-                else:
-                    y = (self.weights[0] - self.weights[1] * x) / self.weights[2]
-            plt.plot(x, y ,"k--")
-        else:
-            for idx, each_weights in enumerate(self.weights):
-                print(each_weights)
-                x = np.linspace(plt.xlim()[0], plt.xlim()[1], 2)
-                y = np.linspace(plt.ylim()[0], plt.ylim()[1], 2)
-                if each_weights[1] == 0:
-                    y = [each_weights[0]/each_weights[2],each_weights[0]/each_weights[2]]
-                elif each_weights[2] == 0:
-                    x = [each_weights[0]/each_weights[1],each_weights[0]/each_weights[1]]
-                else:
-                    print("normal")
-                    #x = [0,self.weights[0]/self.weights[1]]
-                    if abs(-each_weights[1]/each_weights[2]) > 1:
-                        x = (each_weights[0] - each_weights[2] * y) / each_weights[1]
-                    else:
-                        y = (each_weights[0] - each_weights[1] * x) / each_weights[2]
-                plt.plot(x, y ,color[idx] + "--")
-        #print(x, ", ",y)
-
-
-
-        plt.draw()
-        plt.show()
     def __draw_3d(self):
         pass
 
+class MainClass():
+    def __init__(self, canvas):
+        self.window = Gtk.Window()
+        self.window.set_default_size(800, 500)
+        self.boxvertical = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.window.add(self.boxvertical)
 
-
+        # drawing fig
+        self.box = Gtk.Box()
+        self.boxvertical.pack_start(self.box, True, True, 0)
+        self.box.pack_start(canvas, True, True, 0)
+        # self.fig.canvas.draw()
 
 
 
@@ -122,10 +87,16 @@ def main():
     print("drawing function")
     data = dp('/home/shaowu/code/neural_network/dataset/2Ccircle1.txt')
     data.open_file()
-    buf = None
-    p = paper(buf)
-    print(data.get_data())
+    p = paper()
     p.draw_2d_point(data.get_data())
+    p.draw()
+
+    mc = MainClass(p.canvas)
+
+    mc.window.connect("delete-event", Gtk.main_quit)
+    mc.window.show_all()
+    Gtk.main()
+
 
 if __name__ == '__main__':
     main()
