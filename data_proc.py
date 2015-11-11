8920208fabaa69155d896e028063339bc100ea9f
Shaowu <breeze101792@gmail.com>
2015-11-11 23:36:35 +0800

    in data_proc add input normalize

--- data_proc.py
+++ data_proc.py
@@ -17,13 +17,16 @@ class data_proc:
         self.nom_ys = []
         self.class_table = []
         self.class_num = 0
+        self.max_min = None
     def set_file_name(self, file_name):
         self.file_name = file_name
     def open_file(self):
         self.data = []
         self.ys = []
+        self.nom_data = []
         self.nom_ys = []
         self.class_table = []
+        self.max_min = []
 
         tmp_table = myDict()
         self.class_num = 0
@@ -31,9 +34,26 @@ class data_proc:
         with open(self.file_name) as f:
             for line in f:
                 tmp_list = line.split()
+                #init max_min for data normalize
+                if self.max_min == []:
+                    # ([min],[max])
+                    for each_d in range(len(tmp_list) - 1):
+                        self.max_min.append([float(tmp_list[each_d]), float(tmp_list[each_d])])
+
                 if float(tmp_list[-1]) not in self.ys:
                     self.class_num += 1
+
                 # print([float(x) for x in line.split()])
+                for idx_d, each_x in enumerate(tmp_list[0:-1]):
+                    each_x = float(each_x)
+                    print("mm a\t", self.max_min[idx_d])
+                    print("each_x\t", each_x)
+                    if self.max_min[idx_d][0] > each_x:
+                        self.max_min[idx_d][0] = each_x
+                    if self.max_min[idx_d][1] < each_x:
+                        self.max_min[idx_d][1] = each_x
+                    print("mm b\t", self.max_min[idx_d])
+
                 self.data.append([float(x) for x in tmp_list[0:-1]])
                 self.ys.append(float(tmp_list[-1]))
         # print(self.data)
@@ -48,7 +68,7 @@ class data_proc:
         return list(self.class_table.keys())
     def get_data(self, rate_of_data = 1, is_random = True, approach = "class"):
         self.__data_normalize(approach)
-        tmp_data = self.data.copy()
+        tmp_data = self.nom_data.copy()
         tmp_nom_ys = self.nom_ys.copy()
         sub_dataset = []
         sub_ys = []
@@ -76,11 +96,22 @@ class data_proc:
     #approach = class/function
     def __data_normalize(self, approach = "class"):
         if approach == "class":
-            print("class")
+            print("class method")
             self.class_table = myDict()
+            self.nom_data = []
             self.nom_ys = []
             distance = 1 / (self.class_num * 2)
             class_middle = distance
+            print("max_min\t", self.max_min)
+            for idx_data in range(len(self.data)):
+                # print("data\t", self.data[idx_data])
+                tmp_data = []
+                for idx_dim in range(len(self.data[idx_data])):
+                    tmp_data.append((self.data[idx_data][idx_dim] - self.max_min[idx_dim][0]) / (self.max_min[idx_dim][1] - self.max_min[idx_dim][0]))
+                    # print("tmpe_data\t", (self.data[idx_data][idx_dim] - self.max_min[idx_dim][0]) / self.max_min[idx_dim][1])
+                self.nom_data.append(tmp_data)
+            # print(self.nom_data)
+
             for each_y in self.ys:
                 if each_y not in self.class_table.values():
                     self.class_table.add(class_middle, each_y)
