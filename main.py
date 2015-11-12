d6cebdde0a62cd8a332ee60280dd9598e57ecebb
Shaowu <breeze101792@gmail.com>
2015-11-12 23:13:18 +0800

    add mlp structure adjusment and some ui rewrite

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ UI_INFO = """
     <!--toolitem action='FileNewStandard' /-->
     <toolitem action='FileOpen' />
     <!--toolitem action='FileSave' /-->
+    <toolitem action='Run' />
     <toolitem action='FileQuit' />
   </toolbar>
   <!--popup name='PopupMenu'>
@@ -50,12 +51,11 @@ UI_INFO = """
   </popup-->
 </ui>
 """
-
 class MenuExampleWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="Neural Network")
 
-        self.set_default_size(640, 480)
+        self.set_default_size(800, 600)
 
         action_group = Gtk.ActionGroup("my_actions")
 
@@ -80,77 +80,158 @@ class MenuExampleWindow(Gtk.Window):
         body_panel = Gtk.Table(2, 4, True)
         main_ui.pack_start(body_panel, True, True, 0)
 
-        #settings pannel
+        #for testing/*********************************************************/
+        settings_panel = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        body_panel.attach(settings_panel, 0, 1, 0, 1, xpadding=8, ypadding=8)
 
-        settings = Gtk.Table(10, 2, True)
-        body_panel.attach(settings, 0, 1, 0, 1)
+        settings_lab = Gtk.Label("Settings", xalign=0)
+        settings_panel.pack_start(settings_lab, False, False, 0)
 
-        settings_lab = Gtk.Label("Settings")
-        settings.attach(settings_lab, 0, 2, 0, 1)
-
-        learning_rate_lab = Gtk.Label("Learning rate(0.1):")
-        settings.attach(learning_rate_lab, 0, 1, 1, 2)
-
+        # learning rate
+        learning_rate_group = Gtk.Table(1, 2, True)
+        settings_panel.pack_start(learning_rate_group, False, False, 0)
+        learning_rate_lab = Gtk.Label("Learning rate(0.1):", xalign=0)
+        learning_rate_group.attach(learning_rate_lab, 0, 1, 0, 1)
         learning_rate_adj = Gtk.Adjustment(2, 0, 10, 1, 10, 0)
         self.learning_rate_sb = Gtk.SpinButton()
+        self.learning_rate_sb.set_alignment(xalign=1)
         self.learning_rate_sb.set_adjustment(learning_rate_adj)
-        settings.attach(self.learning_rate_sb, 1, 2, 1, 2)
+        learning_rate_group.attach(self.learning_rate_sb, 1, 2, 0, 1)
         self.learning_rate_sb.set_value(4)
 
-        training_times_lab = Gtk.Label("Traning times:")
-        settings.attach(training_times_lab, 0, 1, 2, 3)
-
-        training_times_adj = Gtk.Adjustment(50, 0, 10000, 50, 10, 0)
+        # traning times
+        training_times_group = Gtk.Table(1, 2, True)
+        settings_panel.pack_start(training_times_group, False, False, 0)
+        training_times_lab = Gtk.Label("Traning times:", xalign=0)
+        training_times_group.attach(training_times_lab, 0, 1, 0, 1)
+        training_times_adj = Gtk.Adjustment(50, 0, 10000, 20, 0, 0)
         self.training_times_sb = Gtk.SpinButton()
+        self.training_times_sb.set_alignment(xalign=1)
         self.training_times_sb.set_adjustment(training_times_adj)
-        settings.attach(self.training_times_sb, 1, 2, 2, 3)
-        self.training_times_sb.set_value(100)
+        training_times_group.attach(self.training_times_sb, 1, 2, 0, 1)
+        self.training_times_sb.set_value(50)
 
-        test_lab = Gtk.Label("Find Best:")
-        settings.attach(test_lab, 0, 1, 3, 4)
+        # training_err_rate_group
+        training_err_rate_group = Gtk.Table(1, 2, True)
+        settings_panel.pack_start(training_err_rate_group, False, False, 0)
+        training_err_rate_lab = Gtk.Label("Traning error rate(%):", xalign=0)
+        training_err_rate_group.attach(training_err_rate_lab, 0, 1, 0, 1)
+        training_err_rate_adj = Gtk.Adjustment(10, 0, 100, 5, 0, 0)
+        self.training_err_rate_sb = Gtk.SpinButton()
+        self.training_err_rate_sb.set_alignment(xalign=1)
+        self.training_err_rate_sb.set_adjustment(training_err_rate_adj)
+        training_err_rate_group.attach(self.training_err_rate_sb, 1, 2, 0, 1)
+        self.training_err_rate_sb.set_value(10)
 
-        self.find_best_swh = Gtk.Switch()
-        self.find_best_swh.connect("notify::active", self.on_find_best_activated)
-        self.find_best_swh.set_active(False)
-        settings.attach(self.find_best_swh, 1, 2, 3, 4)
+        # mlp structure
+        mlp_structure_group = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        settings_panel.pack_start(mlp_structure_group, False, False, 0)
+        mlp_structure_lab = Gtk.Label("Specify mlp structure(3,5,1...)", xalign = 0)
+        mlp_structure_group.pack_start(mlp_structure_lab, True, True, 0)
+        self.mlp_structure_ety = Gtk.Entry()
+        self.mlp_structure_ety.set_text("2,2,1")
+        mlp_structure_group.pack_start(self.mlp_structure_ety, True, True, 0)
 
+        # #find best
+        # find_best_group = Gtk.Table(1, 2, True)
+        # settings_panel.pack_start(find_best_group, False, False, 0)
+        # test_lab = Gtk.Label("Find Best:", xalign=0)
+        # find_best_group.attach(test_lab, 0,1,0,1)
+        # self.find_best_swh = Gtk.Switch()
+        # self.find_best_swh.connect("notify::active", self.on_find_best_activated)
+        # self.find_best_swh.set_active(False)
+        # find_best_group.attach(self.find_best_swh, 1,2,0,1)
+
+        # action buttom
+        action_group = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        settings_panel.pack_start(action_group, False, False, 0)
 
         tarin_button = Gtk.Button(label = "Train")
         tarin_button.connect("clicked", self.on_clicked_train)
-        settings.attach(tarin_button, 0, 1, 9, 10)
+        action_group.pack_start(tarin_button, False, False, 0)
 
         draw_button = Gtk.Button(label = "Draw")
         draw_button.connect("clicked", self.on_clicked_draw)
-        settings.attach(draw_button, 1, 2, 9, 10)
+        action_group.pack_start(draw_button, False, False, 0)
+
+
+
+        # /*******************************************************************/
+        # listbox = Gtk.ListBox()
+        # listbox.set_selection_mode(Gtk.SelectionMode.NONE)
+        # body_panel.attach(listbox, 0, 1, 0, 1)
+
+        #settings pannel
+        #
+        # settings = Gtk.Table(10, 2, True)
+        # body_panel.attach(settings, 0, 1, 0, 1)
+        #
+        # settings_lab = Gtk.Label("Settings")
+        # settings.attach(settings_lab, 0, 2, 0, 1)
+        #
+        # learning_rate_lab = Gtk.Label("Learning rate(0.1):")
+        # settings.attach(learning_rate_lab, 0, 1, 1, 2)
+        #
+        # learning_rate_adj = Gtk.Adjustment(2, 0, 10, 1, 10, 0)
+        # self.learning_rate_sb = Gtk.SpinButton()
+        # self.learning_rate_sb.set_adjustment(learning_rate_adj)
+        # settings.attach(self.learning_rate_sb, 1, 2, 1, 2)
+        # self.learning_rate_sb.set_value(4)
+        #
+        # training_times_lab = Gtk.Label("Traning times:")
+        # settings.attach(training_times_lab, 0, 1, 2, 3)
+        #
+        # training_times_adj = Gtk.Adjustment(50, 0, 10000, 50, 10, 0)
+        # self.training_times_sb = Gtk.SpinButton()
+        # self.training_times_sb.set_adjustment(training_times_adj)
+        # settings.attach(self.training_times_sb, 1, 2, 2, 3)
+        # self.training_times_sb.set_value(100)
+        #
+        # test_lab = Gtk.Label("Find Best:")
+        # settings.attach(test_lab, 0, 1, 3, 4)
+        #
+        # self.find_best_swh = Gtk.Switch()
+        # self.find_best_swh.connect("notify::active", self.on_find_best_activated)
+        # self.find_best_swh.set_active(False)
+        # settings.attach(self.find_best_swh, 1, 2, 3, 4)
+        #
+        #
+        # tarin_button = Gtk.Button(label = "Train")
+        # tarin_button.connect("clicked", self.on_clicked_train)
+        # settings.attach(tarin_button, 0, 1, 9, 10)
+        #
+        # draw_button = Gtk.Button(label = "Draw")
+        # draw_button.connect("clicked", self.on_clicked_draw)
+        # settings.attach(draw_button, 1, 2, 9, 10)
 
         #output pannel
         nu_log = Gtk.Table(10, 2, True)
         body_panel.attach(nu_log, 1, 2, 0, 1)
 
-        log_lab = Gtk.Label("Log")
+        log_lab = Gtk.Label("Log", xalign=0)
         nu_log.attach(log_lab, 0, 2, 0, 1)
 
-        class_num_lab = Gtk.Label("Class Number:")
+        class_num_lab = Gtk.Label("Class Number:", xalign=0)
         self.class_num_value_lab = Gtk.Label("0")
         nu_log.attach(class_num_lab, 0, 1, 1, 2)
         nu_log.attach(self.class_num_value_lab, 1, 2, 1, 2)
 
-        err_rate_lab = Gtk.Label("Error rate:")
+        err_rate_lab = Gtk.Label("Error rate:", xalign=0)
         self.err_rate_value_lab = Gtk.Label("0%")
         nu_log.attach(err_rate_lab, 0, 1, 2, 3)
         nu_log.attach(self.err_rate_value_lab, 1, 2, 2, 3)
 
-        itimes_lab = Gtk.Label("Iteration Times:")
+        itimes_lab = Gtk.Label("Iteration Times:", xalign=0)
         self.itimes_value_lab = Gtk.Label("0")
         nu_log.attach(itimes_lab, 0, 1, 3, 4)
         nu_log.attach(self.itimes_value_lab, 1, 2, 3, 4)
 
-        num_data_lab = Gtk.Label("Number of data:")
+        num_data_lab = Gtk.Label("Number of data:", xalign=0)
         self.num_data_value_lab = Gtk.Label("0")
         nu_log.attach(num_data_lab, 0, 1, 4, 5)
         nu_log.attach(self.num_data_value_lab, 1, 2, 4, 5)
 
-        best_times_lab = Gtk.Label("Best result:")
+        best_times_lab = Gtk.Label("Best result:", xalign=0)
         self.best_times_value_lab = Gtk.Label("(0, 0)")
         nu_log.attach(best_times_lab, 0, 1, 5, 6)
         nu_log.attach(self.best_times_value_lab, 1, 2, 5, 6)
@@ -188,10 +269,7 @@ class MenuExampleWindow(Gtk.Window):
         self.training_set = []
         self.testing_set = []
         self.trainsformed_data = []
-    # def ui_img_panel(self, container):
-    #     nu_log = Gtk.image(10, 2, True)
-    #     container.attach(nu_log, 1, 2, 0, 1)
-    #     pass
+        # self.classifier = None
     def add_file_menu_actions(self, action_group):
         action_filemenu = Gtk.Action("FileMenu", "File", None, None)
         action_group.add_action(action_filemenu)
@@ -217,6 +295,10 @@ class MenuExampleWindow(Gtk.Window):
 
         action_filesave = Gtk.Action("FileSave", "Save", None, Gtk.STOCK_SAVE)
         action_group.add_action(action_filesave)
+
+        action_run = Gtk.Action("Run", "Run", None, Gtk.STOCK_MEDIA_PLAY)
+        action_run.connect("activate", self.on_clicked_train)
+        action_group.add_action(action_run)
 
         action_filequit = Gtk.Action("FileQuit", None, None, Gtk.STOCK_QUIT)
         action_filequit.connect("activate", self.on_menu_file_quit)
@@ -262,7 +344,7 @@ class MenuExampleWindow(Gtk.Window):
             self.training_set = self.dataset.get_data(1)
             self.training_set = (dp.to_ndata(self.training_set[0]), self.training_set[1])
             self.testing_set = self.training_set
-            print(self.training_set)
+            # print(self.training_set)
             self.ori_paper.draw_2d_point(self.training_set)
             # self.training_set = self.dataset.get_data(0.3)
             # self.testing_set = self.dataset.get_another_data(self.training_set[0])
@@ -293,15 +375,19 @@ class MenuExampleWindow(Gtk.Window):
         else:
             self.find_best = False
     def on_clicked_train(self, widget):
-        nn = mlp(structure = [2,1], dimension = len(self.training_set[0][0]), class_middle = self.dataset.get_class_middle(), learning_rate = self.learning_rate_sb.get_value(), training_times = int(self.training_times_sb.get_value()), best_w = self.find_best)
+        tmp_struct = self.mlp_structure_ety.get_text()
+
+        print()
+        nn = mlp(structure = [int(x) for x in tmp_struct.split(',')], dimension = len(self.training_set[0][0]), err_rate = self.training_err_rate_sb.get_value(),class_middle = self.dataset.get_class_middle(), learning_rate = self.learning_rate_sb.get_value(), training_times = int(self.training_times_sb.get_value()), best_w = self.find_best)
         self.trainsformed_data = nn.training(self.training_set)
         err, self.trainsformed_data = nn.get_err_rate(self.testing_set)
         self.err_rate_value_lab.set_text(str(err * 100) + "%")
-        # print(self.trainsformed_data)
         self.weights = [nn.get_weights()]
-        # print("weight", self.weights)
 
         self.nned_paper.draw_2d_point(self.trainsformed_data)
+
+
+
         # mnn = perceptron(len(self.training_set[0][0]), learning_rate = self.learning_rate_sb.get_value(), training_times = int(self.training_times_sb.get_value()), best_w = self.find_best)
         # mnn.training(self.training_set)
         # self.weights = [mnn.get_weights()]
@@ -384,30 +470,30 @@ class MenuExampleWindow(Gtk.Window):
 
         plt.draw()
         plt.show()
-
-    def draw_3dfig(self):
-        plt.ion()
-        plt.plot(self.data[0], self.data[1], self.data[1], "ro")
-        plt.show(block=False)
-        pass
-
+    #
+    # def draw_3dfig(self):
+    #     plt.ion()
+    #     plt.plot(self.data[0], self.data[1], self.data[1], "ro")
+    #     plt.show(block=False)
+    #     pass
+    #
     def on_menu_others(self, widget):
         print("Menu item " + widget.get_name() + " was selected")
-
-    def on_menu_choices_changed(self, widget, current):
-        print(current.get_name() + " was selected.")
-
-    def on_menu_choices_toggled(self, widget):
-        if widget.get_active():
-            print(widget.get_name() + " activated")
-        else:
-            print(widget.get_name() + " deactivated")
-
-    def on_button_press_event(self, widget, event):
-        # Check if right mouse button was preseed
-        if event.type == Gdk.EventType.BUTTON_PRESS and event.button == 3:
-            self.popup.popup(None, None, None, None, event.button, event.time)
-            return True # event has been handled
+    #
+    # def on_menu_choices_changed(self, widget, current):
+    #     print(current.get_name() + " was selected.")
+    #
+    # def on_menu_choices_toggled(self, widget):
+    #     if widget.get_active():
+    #         print(widget.get_name() + " activated")
+    #     else:
+    #         print(widget.get_name() + " deactivated")
+    #
+    # def on_button_press_event(self, widget, event):
+    #     # Check if right mouse button was preseed
+    #     if event.type == Gdk.EventType.BUTTON_PRESS and event.button == 3:
+    #         self.popup.popup(None, None, None, None, event.button, event.time)
+    #         return True # event has been handled
 
 def main():
     window = MenuExampleWindow()
