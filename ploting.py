2c47f679eb2f3321b4aa802ed9a1b16061256864
Shaowu <breeze101792@gmail.com>
2015-11-13 23:55:47 +0800

    add reshape() when __init__ in ploting!

--- ploting.py
+++ ploting.py
@@ -8,33 +8,16 @@ import numpy as np
 from matplotlib.figure import Figure
 #draw 2d: points, lines, axises
 class paper:
-    def __init__(self, pix_buf = None):
+    def __init__(self, unit_xy = (10, 10)):
         self.color = ["r","b","g","c","m","y","k","w"]
         self.data = None
         # self.fig = plt.figure()
-        self.pix_buf = pix_buf
 
-        self.fig = Figure(figsize=(10,10), dpi=100)
+        self.fig = Figure(figsize = unit_xy, dpi=100)
         self.ax = self.fig.add_subplot(111)
         self.canvas = FigureCanvas(self.fig)
+        self.resetpaper()
 
-    def drawing(self):
-        fig = plt.figure(figsize=(100,100),dpi=75)
-        ax1 = fig.add_axes([0.1, 0.1, 0.4, 0.7])
-        ax2 = fig.add_axes([0.55, 0.1, 0.4, 0.7])
-        x = np.arange(0.0, 2.0, 0.02)
-        y1 = np.sin(2*np.pi*x)
-        y2 = np.exp(-x)
-        l1, l2 = ax1.plot(x, y1, 'rs-', x, y2, 'go')
-
-        y3 = np.sin(4*np.pi*x)
-        y4 = np.exp(-2*x)
-        l3, l4 = ax2.plot(x, y3, 'yd-', x, y4, 'k^')
-
-        # fig.legend((l1), ("test"), 'upper right')
-        # fig.legend((l3, l4), ('Line 3', 'Line 4'), 'upper right')
-        plt.show()
-        print(fig.get_figure())
     def resetpaper(self):
         self.ax.cla()
         # self.ax.set_xlim(0,10)
@@ -64,6 +47,41 @@ class paper:
         for idx in range(len(class_list)):
             self.ax.plot(tmp_point[idx][0], tmp_point[idx][1], self.color[idx] + "o")
         self.fig.canvas.draw()
+    #TODO
+    def draw_2d_line(self, slope, bias):
+        # if self.weights == 1:
+        #     x = np.linspace(plt.xlim()[0], plt.xlim()[1], 2)
+        #     y = np.linspace(plt.ylim()[0], plt.ylim()[1], 2)
+        #     if self.weights[1] == 0:
+        #         y = [self.weights[0]/self.weights[2],self.weights[0]/self.weights[2]]
+        #     elif self.weights[2] == 0:
+        #         x = [self.weights[0]/self.weights[1],self.weights[0]/self.weights[1]]
+        #     else:
+        #         #x = [0,self.weights[0]/self.weights[1]]
+        #         print("draw")
+        #         if -self.weights[1]/self.weights[2] > 1:
+        #             print(" > ")
+        #             x = (self.weights[0] - self.weights[2] * x) / self.weights[1]
+        #         else:
+        #             y = (self.weights[0] - self.weights[1] * x) / self.weights[2]
+        #     plt.plot(x, y ,"k--")
+        # else:
+        #     for idx, each_weights in enumerate(self.weights):
+        #         print(each_weights)
+        #         x = np.linspace(plt.xlim()[0], plt.xlim()[1], 2)
+        #         y = np.linspace(plt.ylim()[0], plt.ylim()[1], 2)
+        #         if each_weights[1] == 0:
+        #             y = [each_weights[0]/each_weights[2],each_weights[0]/each_weights[2]]
+        #         elif each_weights[2] == 0:
+        #             x = [each_weights[0]/each_weights[1],each_weights[0]/each_weights[1]]
+        #         else:
+        #             print("normal")
+        #             #x = [0,self.weights[0]/self.weights[1]]
+        #             if abs(-each_weights[1]/each_weights[2]) > 1:
+        #                 x = (each_weights[0] - each_weights[2] * y) / each_weights[1]
+        #             else:
+        #                 y = (each_weights[0] - each_weights[1] * x) / each_weights[2]
+        #         plt.plot(x, y ,color[idx] + "--")
 
     def __draw_3d(self):
         pass
