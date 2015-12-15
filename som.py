326b042fc88dcc0e046d256a73b89bf7404231f4
Shaowu <breeze101792@gmail.com>
2015-12-16 01:42:35 +0800

    1.fix using wrong data to calc distance 2.add drawing paper when net update

--- som.py
+++ som.py
@@ -1,15 +1,17 @@
 import numpy as np
 from ploting import paper
-
+import threading
 # for 2 dimension
 class SOM:
-    def __init__(self, i_len = 100, j_len = 100, sd = 1):
+    def __init__(self, i_len = 100, j_len = 100, sd = 1, learning_rate = 0.4, training_times = 5):
         self.net = None
         self.dimension = 2
         self.i_len = i_len
         self.j_len = j_len
-        self.learning_rate = 0.1
+        self.learning_rate = learning_rate
         self.sdp = (sd ** 2) * 2
+        self.training_times = training_times
+        print(i_len, j_len, sd, learning_rate, training_times)
     def net_init(self):
         self.net = [[] for _ in range(self.i_len)]
         for i in range(self.i_len):
@@ -31,18 +33,23 @@ class SOM:
     def net_update(self, point, winner):
         for i in range(self.i_len):
             for j in range(self.j_len):
-                distance = np.linalg.norm(winner - np.array([i, j]))# is wrong here TODO
+                distance = np.linalg.norm(winner - np.array([i, j]))
                 # print(self.net[winner[0]][winner[1]] - self.net[i][j], ", ", np.exp(-(distance ** 2)/self.sdp ), ", ", point)
                 self.net[i][j] = self.net[i][j] + self.learning_rate * np.exp(-(distance ** 2)/self.sdp ) * (point - self.net[i][j])
-    def training(self, data):
+    def training(self, data, drawf):
         winner = None
         dis = 0
         counter = 0
-
-        while counter < 50:
+        lock = [False]
+        while counter < self.training_times:
             for p in data:
                 winner = self.whowin(p)
                 self.net_update(p, winner)
+                if not lock[0]:
+                    lock[0] = True
+                    draw_thread = threading.Thread(target=drawf(self.net, lock))
+                    draw_thread.start()
+                    draw_thread.join()
             counter += 1
     def get_net(self):
         return self.net
