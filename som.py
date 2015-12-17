fa9d45d4be59e6535c52e11e3a84d02a38f87f89
Shaowu <breeze101792@gmail.com>
2015-12-17 20:11:16 +0800

    reimplement whowin method with multi thread way

--- som.py
+++ som.py
@@ -1,7 +1,9 @@
 import numpy as np
 from ploting import paper
 import threading
+from multiprocessing import Pool, Array, Queue, Process
 # for 2 dimension
+
 class SOM:
     def __init__(self, i_len = 100, j_len = 100, sd = 1, learning_rate = 0.4, training_times = 5):
         self.net = None
@@ -12,23 +14,72 @@ class SOM:
         self.sdp = (sd ** 2) * 2
         self.training_times = training_times
         self.threshold = 0.001
+
         print(i_len, j_len, sd, learning_rate, training_times)
     def net_init(self):
         self.net = [[] for _ in range(self.i_len)]
         for i in range(self.i_len):
             self.net[i] = [np.array((i / (self.i_len - 1), j / (self.j_len - 1)))for j in range(self.j_len)]
-    def whowin(self, point):
+    def __whowin_dq(self, point, i_start, i_end, que):
         winner = np.array([0,0])
         winner_ip = 100
         cur_ip = 0
-        for i in range(self.i_len):
+        for i in range(i_start, i_end):
             for j in range(self.j_len):
                 cur_ip = np.linalg.norm((point - (self.net[i][j])) if np.sum(self.net[i][j]) != 0 else point)
                 if winner_ip > cur_ip:
                     winner_ip = cur_ip
                     winner[0] = i
                     winner[1] = j
+        que.put(winner)
+
+    def whowin(self, point):
+        cur_ip = 0
+        core_number = 16
+        # buf = [np.array([-1,-1]) for _ in range(core_number)]
+        que = Queue()
+        disp = int(self.i_len / core_number)
+        counter = 0
+
+        # processes = [Process(target=self.__whowin_dq, args=(point, counter, counter + disp, idx, que)) for idx in range(core_number)]
+        processes = []
+        for idx in range(core_number):
+            if idx == core_number - 1:
+                processes.append(Process(target=self.__whowin_dq, args = (point, counter, self.i_len, que)))
+            else:
+                processes.append(Process(target=self.__whowin_dq, args = (point, counter, counter + disp, que)))
+            counter += disp
+        for p in processes:
+            p.start()
+        for p in processes:
+            p.join()
+        buf = [que.get() for p in processes]
+        winner = buf[0]
+        winner_ip = np.linalg.norm((point - (self.net[buf[0][0]][buf[0][1]])) if np.sum(self.net[buf[0][0]][buf[0][1]]) != 0 else point)
+        for idx in range(1, core_number):
+            cur_ip = np.linalg.norm((point - (self.net[buf[idx][0]][buf[idx][1]])) if np.sum(self.net[buf[idx][0]][buf[idx][1]]) != 0 else point)
+            if winner_ip > cur_ip:
+                winner_ip = cur_ip
+                winner = buf[idx]
+        bwinner = winner
+        #
+        # print(buf)
+        # print(winner)
+        #
+        # winner = np.array([0,0])
+        # winner_ip = 100
+        # for i in range(self.i_len):
+        #     for j in range(self.j_len):
+        #         cur_ip = np.linalg.norm((point - (self.net[i][j])) if np.sum(self.net[i][j]) != 0 else point)
+        #         if winner_ip > cur_ip:
+        #             winner_ip = cur_ip
+        #             winner[0] = i
+        #             winner[1] = j
         # print("winner", winner[0], ", ",winner[1])
+        # if np.linalg.norm(winner - bwinner) != 0:
+        #     print(buf)
+        #     print(winner, bwinner)
+        #     input("press enter")
         return winner
         # return np.array([0 ,0])
     def net_update(self, point, winner):
@@ -71,7 +122,7 @@ class SOM:
                 j_end = idx
                 break
 
-        print("update", i_start, i_end, j_start, j_end)
+        # print("update", i_start, i_end, j_start, j_end)
         for i in range(i_start, i_end + 1):
             for j in range(j_start, j_end + 1):
                 distance = np.linalg.norm(winner - np.array([i, j]))
@@ -92,7 +143,7 @@ class SOM:
                     lock[0] = True
                     draw_thread = threading.Thread(target=drawf(self.net, lock))
                     draw_thread.start()
-                    draw_thread.join()
+
             counter += 1
     def get_net(self):
         return self.net
